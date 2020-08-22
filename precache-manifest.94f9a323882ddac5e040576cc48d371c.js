d677072376fcc02324fa1d06c037e066720d1687
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2020-08-22 14:43:15 +0200

    Updates

---
+++ precache-manifest.94f9a323882ddac5e040576cc48d371c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfd95888d948974a9cfa76c084f725a",
+    "url": "/TodoList-with-react/index.html"
+  },
+  {
+    "revision": "e49ce7c33ba9f951dc88",
+    "url": "/TodoList-with-react/static/js/2.4fde0391.chunk.js"
+  },
+  {
+    "revision": "0c716ecb21e91c590beb",
+    "url": "/TodoList-with-react/static/js/main.a2280187.chunk.js"
+  },
+  {
+    "revision": "ab4e45efaeeb5c29f826",
+    "url": "/TodoList-with-react/static/js/runtime-main.0b6289f4.js"
+  }
+]);
